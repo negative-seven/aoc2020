d1e86619a18c4f9e17605f25e27c02a9810f3721
negative-seven <tehseven1@gmail.com>
2020-12-07 15:37:32 +0100

    Day 7 partial rewrite

--- days/day7.py
+++ days/day7.py
@@ -12,31 +12,28 @@ def convert_input(raw_input):
 	return bag_direct_contents
 
 def run_first(bag_direct_contents):
-	bag_direct_outer_containers = defaultdict(set)
-	for outer_bag in bag_direct_contents:
-		for inner_bag in bag_direct_contents[outer_bag]:
-			bag_direct_outer_containers[inner_bag].add(outer_bag)
-
-	return len(get_bag_outer_containers_recursively('shiny gold', bag_direct_outer_containers))
+	bag_contents = generate_bag_contents(bag_direct_contents)
+	return sum('shiny gold' in inner_bags for inner_bags in bag_contents.values())
 
 def run_second(bag_direct_contents):
-	return sum(get_bag_contents_with_counts_recursively('shiny gold', bag_direct_contents).values())
+	bag_contents = generate_bag_contents(bag_direct_contents)
+	return sum(bag_contents['shiny gold'].values())
 
-def get_bag_outer_containers_recursively(bag, bag_direct_outer_containers):
-	outer_containers = set()
-	for outer_bag in bag_direct_outer_containers[bag]:
-		outer_containers.add(outer_bag)
-		try:
-			outer_containers.update(get_bag_outer_containers_recursively(outer_bag, bag_direct_outer_containers))
-		except KeyError:
-			pass
-	return outer_containers
+def generate_bag_contents(bag_direct_contents):
+	bag_contents = defaultdict(Counter)
 
-def get_bag_contents_with_counts_recursively(bag, bag_direct_contents):
-	contents = Counter()
-	for inner_bag, inner_bag_count in bag_direct_contents[bag].items():
-		inner_bag_contents = get_bag_contents_with_counts_recursively(inner_bag, bag_direct_contents)
-		for b in inner_bag_contents:
-			contents[b] += inner_bag_contents[b] * inner_bag_count
-		contents[inner_bag] += inner_bag_count
-	return contents
+	def get_one_bag_contents_recursively(bag):
+		if bag not in bag_contents:
+			for inner_bag, inner_bag_count in bag_direct_contents[bag].items():
+				inner_bag_contents = get_one_bag_contents_recursively(inner_bag)
+				for b in inner_bag_contents:
+					bag_contents[bag][b] += inner_bag_contents[b] * inner_bag_count
+				bag_contents[bag][inner_bag] += inner_bag_count
+
+		return bag_contents[bag]
+
+
+	for bag in bag_direct_contents:
+		get_one_bag_contents_recursively(bag)
+
+	return bag_contents
