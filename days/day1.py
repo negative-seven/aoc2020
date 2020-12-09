12c7d50bdf9fba4455023f7314d0e302f79ae549
negative-seven <tehseven1@gmail.com>
2020-12-09 13:29:12 +0100

    Change a var name in day 1

--- days/day1.py
+++ days/day1.py
@@ -12,6 +12,6 @@ def run_second(puzzle_input):
 	return find_product_of_summing_numbers(puzzle_input, 3, 2020)
 
 def find_product_of_summing_numbers(numbers, numbers_count, target_sum):
-	for pair in itertools.combinations(numbers, numbers_count):
-		if sum(pair) == target_sum:
-			return math.prod(pair)
+	for combination in itertools.combinations(numbers, numbers_count):
+		if sum(combination) == target_sum:
+			return math.prod(combination)
